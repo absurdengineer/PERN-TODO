faee748089d09350823f68bdd79f1866ec8c3dda
mddalam1 <mddalam1@gmail.com>
2020-12-15 12:36:13 +0530

    Set up Routes for Todos.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 //* Loading Modules
 const express = require('express')
 const cors = require('cors')
-const pool = require('./database/database.connect')
+const todo = require('./routes/apis/todo.api')
+
+//* Creating App
 const app = express()
 
 //* Middlewares
 app.use(cors())
 app.use(express.json())
+
+//* Routes 
+app.use('/api/todos/', todo)
 
 //* App Configuration Settings
 const port = process.env.PORT || 8080
